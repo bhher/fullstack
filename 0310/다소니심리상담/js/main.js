c6b819c3bf29bb0c67f0efae692e9320490bb1be
bhher30 <bhher30@gmail.com>
2025-03-11 18:25:46 +0900

    0311a

--- 0310/다소니심리상담/js/main.js
+++ 0310/다소니심리상담/js/main.js
@@ -22,4 +22,79 @@ $(function(){
         }
     });
 
+var a1 = $(".s2_title img").offset().top;  // '/'
+var a2 = $('.s2_title h2').offset().top; // 제목
+var a3 = $('.s2_title p').offset().top; // 내용
+var a4 = $('.s2_table li').offset().top;
+
+
+var b1 = $(".s3_title img").offset().top;  // '/'
+var b2 = $('.s3_title h2').offset().top; // 제목
+var b3 = $('.s3_title p').offset().top; // 내용
+var b4 = $('.s3_table li').offset().top;
+
+
+var d1 = $('#s5').offset().top;
+var d2 = $('#s6').offset().top;
+console.log(a1,a2,a3);
+
+$(window).scroll(function(){
+   var sct = $(this).scrollTop();
+    if(a1 <= sct + 700) {
+        $(".s2_title img").addClass('slide');
+    }   
+    if(a2 < sct + 700){
+        $('.s2_title h2').addClass('slide');
+    }
+    if(a3 < sct + 700){
+     $('.s2_title p').addClass('slide');
+    }
+    if(a4 < sct + 700){
+        $('.s2_table li').eq(0).addClass('slide');
+        setTimeout(function(){
+            $('.s2_table li').eq(1).addClass('slide');
+        },300);
+        setTimeout(function(){
+            $('.s2_table li').eq(2).addClass('slide');
+        },600);
+        setTimeout(function(){
+            $('.s2_table li').eq(3).addClass('slide');
+        },900);
+     }
+
+     if(b1 <= sct + 700) {
+        $(".s3_title img").addClass('slide');
+    }   
+    if(b2 < sct + 700){
+        $('.s3_title h2').addClass('slide');
+    }
+    if(b3 < sct + 700){
+     $('.s3_title p').addClass('slide');
+    }
+    if(b4 < sct + 700){
+        $('.s3_table li').eq(0).addClass('slide');
+        setTimeout(function(){
+            $('.s3_table li').eq(1).addClass('slide');
+        },300);
+        setTimeout(function(){
+            $('.s3_table li').eq(2).addClass('slide');
+        },600);
+        setTimeout(function(){
+            $('.s3_table li').eq(3).addClass('slide');
+        },900);
+        setTimeout(function(){
+            $('.s3_table li').eq(4).addClass('slide');
+        },1200);
+     }
+
+     if(d1 <= sct + 700) {
+        $(".s5_inner").addClass('slide1');
+    }  
+    if(d2 <= sct + 700) {
+        $(".s6_noti, .s6_review").addClass('slide1');
+    }  
+
+});
+
+
 });
